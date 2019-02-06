d110b2dac3acd5b9c47fd35ff4df0bd305ab5a69
William Falcone <wjf0003@gmail.com>
2019-02-06 13:26:38 -0500

    j

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+
+function component(){
+  let element = document.createElement('div');
+
+  element.innerHTML = _.join(['Hello' , 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
