d6377bef65fa17d459ae7957944ffafe51698b00
William Falcone <wjf0003@gmail.com>
2019-02-10 16:04:01 -0500

    background has been added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 document.addEventListener("DOMContentLoaded", () =>{
   let canvas = document.getElementById("game-canvas")
   let ctx = canvas.getContext('2d');
+
+  let backImg = new Image();
+  backImg.src = '/Users/william_falcone/Desktop/javascript_game/assets/images/marioclouds2.jpg';
+  let cW = ctx.canvas.width;
+  let cH = ctx.canvas.height;
+
+  function Background(){
+    this.x = 0, this.y = 0, this.w = backImg.width, this.h = backImg.height;
+    this.render = function(){
+      ctx.drawImage(backImg, this.x --, 0);
+      if(this.x <= -499){
+        this.x = 0;
+      }
+    }
+  }
+
+
+  let background = new Background();
   
   let ballX = 600
-  let ballY = 450;
+  let ballY = Math.floor(Math.random() * (500 - 300) + 300) //450;
   let ballRad = 5
-  let ballSpeed = -5
+  let ballSpeed = -3
+
+  let coinX = 600;
+  let coinY = 300
 
   let rectPosY = 250
   let rectPosX = 150
   let spacePressed = false;
+
+  const posResetY = Math.floor(Math.random() * (500 - 300) + 300);
+
+  const jerry = new Image();
+  jerry.src ='/Users/william_falcone/Desktop/javascript_game/assets/images/jerry2.jpg';
 
 
   let score = 0;
@@ -19,6 +45,21 @@ document.addEventListener("DOMContentLoaded", () =>{
     if(ballX < 0 || ballX > canvas.width){
       score += 1
       ballSpeed = -ballSpeed
+    }
+  }
+
+  function restartBall(){
+    if (ballX < 0){
+      ballX = canvas.width;
+      ballY = Math.floor(Math.random() * (500 - 300) + 300)
+      
+    }
+  }
+
+  function restartCoin(){
+    if(coinX < 0){
+      coinX = canvas.width;
+      coinY = Math.floor(Math.random() * (500 - 300) + 300)
     }
   }
 
@@ -37,6 +78,8 @@ document.addEventListener("DOMContentLoaded", () =>{
     }
   }
 
+  
+
 
   function drawScore(){
     ctx.font ='16px Arial';
@@ -44,15 +87,29 @@ document.addEventListener("DOMContentLoaded", () =>{
     ctx.fillText("Score: "+score, 100, 100)
   }
 
-  function hitBlock(){
+  function coinHitBlock(){
     
-    if ((ballX === rectPosX && (ballY > rectPosY && ballY < rectPosY + 100)) || (ballX === rectPosX + 50 && (ballY > rectPosY && ballY < rectPosY + 100))) {
-      score = 0
+    if ((coinX === rectPosX && (coinY > rectPosY && coinY < rectPosY + 100)) || (coinX === rectPosX + 50 && (coinY > rectPosY && coinY < rectPosY + 100))) {
+      score += 1
+      coinX = canvas.width
+      coinY = Math.floor(Math.random() * (500 - 300) + 300)
     }
   }
+
+  function ballHitBlock() {
+
+    if ((ballX === rectPosX && (ballY > rectPosY && ballY < rectPosY + 100)) || (ballX === rectPosX + 50 && (ballY > rectPosY && ballY < rectPosY + 100))) {
+      score = 0
+      ballX = canvas.width
+      ballY = Math.floor(Math.random() * (500 - 300) + 300)
+    }
+  }
+
+
  
 
   function rect(){
+ 
     ctx.beginPath();
     ctx.rect(rectPosX, rectPosY, 50, 100);
     ctx.fillStyle = "red";
@@ -60,10 +117,19 @@ document.addEventListener("DOMContentLoaded", () =>{
     ctx.closePath();
   }
 
+  function coin(){
+    ctx.beginPath();
+    ctx.arc(coinX, coinY, ballRad, 0, Math.PI * 2);
+    ctx.fillStyle = 'green';
+    ctx.fill();
+    ctx.closePath();
+  }
+
   
   function draw(){
-  
+    ctx.save();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    background.render();
 
     //ball
     ctx.beginPath();
@@ -73,6 +139,7 @@ document.addEventListener("DOMContentLoaded", () =>{
     ctx.closePath();
     
     
+    ctx.drawImage(jerry,150,100,50,100);
     
     // ctx.beginPath();
     // ctx.rect(350, 200, 50, 100);
@@ -83,21 +150,31 @@ document.addEventListener("DOMContentLoaded", () =>{
     rect();
 
     drawScore();
+    coin();
+    restartCoin();
+
+
 
     
     
     //ball moving
     ballX+= ballSpeed
+    coinX+= ballSpeed
 
  
-    bouncWall();
-    hitBlock();
+    // bouncWall();
+    coinHitBlock();
+    ballHitBlock();
+    restartBall();
     
     
     requestAnimationFrame(draw)
     
-   if(spacePressed && rectPosY >0){
-     rectPosY -= 5;
+   if(spacePressed && rectPosY >300){
+     rectPosY -= 20;
+     
+
+
    } 
 
    if(!spacePressed && rectPosY < canvas.height-100){
