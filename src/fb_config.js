77dc890d3b3367ae7a5ce0917382dd9410d7bdc8
William Falcone <wjf0003@gmail.com>
2024-06-04 09:26:56 -0400

    update

---
+++ src/fb_config.js
@@ -0,0 +1,16 @@
+const f_config = {
+  apiKey: "AIzaSyD8Y8oW1J4VAZhkhaAIX_pknRCZKhSrLMI",
+  authDomain: "jerry-run-a963a.firebaseapp.com",
+  databaseURL: "https://jerry-run-a963a.firebaseio.com",
+  projectId: "jerry-run-a963a",
+  storageBucket: "jerry-run-a963a.appspot.com",
+  messagingSenderId: "401633777554",
+  appId: "1:401633777554:web:a01bfc9b525b7dc5ed134b",
+  measurementId: "G-BYM8RGB5D7"
+}
+
+// this is the configu
+
+
+
+export default f_config;
